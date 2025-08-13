6eb3603337cbe3774a8f915eeae889cf8fb63acc
bootstrapbool <bootstrapbool@gmail.com>
2025-08-13 02:46:05 +0000

    Adds various configs

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require('user.options');
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+
+vim.o.ruler = true
+
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
